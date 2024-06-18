037cb420c5be6b31faf4935a17e8f1b1d0862130
Cataldo Federico <fedex46@outlook.it>
2024-06-18 13:42:45 +0200

    frontend: creato 'ProdottoModel'

---
+++ app/src/main/java/it/unical/ea/lemu_frontend/viewModels/ProdottoModel.kt
@@ -0,0 +1,15 @@
+package it.unical.ea.lemu_frontend.viewModels
+
+data class ProductInfo(
+    val id: Int,
+    val imageResId: Int,
+    val name: String,
+    val description: String,
+    val price: String,
+    val venduti: Int,
+    val valutazione: Double,
+    val numeroRecensioni: Int,
+    val category: String,
+    val seller: String,
+    val availability: Boolean
+)
